259c1e8fa55939de26e5882bc7ea17dbfab24c75
Steve Lord <steve@haxors.club>
2017-04-26 12:51:37 +0100

    Added projects

---
+++ examples/4. Developing HIDIOT/Mini Projects/1. Pretty Patterns/blink-waiton.ino
@@ -0,0 +1,18 @@
+// setup runs once on boot:
+
+const int ledPin = 1;
+int waitOn = 1000; // 1 Second
+int waitOff = 500; // 0.5 Seconds
+
+void setup() {                
+  // Tell the ATTiny that we want to use pin 1 as an output
+  pinMode(ledPin, OUTPUT); // Our LED is pin 1 and we're supplying electricity to it.
+}
+
+// loop runs forever and ever:
+void loop() {
+  digitalWrite(ledPin, HIGH); // Make the LED turn on
+  delay(waitOn);               // wait
+  digitalWrite(ledPin, LOW ); // Make the LED turn off
+  delay(waitOff);               // wait
+}
---
+++ examples/4. Developing HIDIOT/Mini Projects/3. Blinking Codes/blink-codes.ino
@@ -0,0 +1,27 @@
+// setup runs once on boot:
+
+const int ledPin = 1;
+const int numberDelay = 2000; // Wait two seconds betwen random numbers
+int waitOn = 500; // 0.5 Seconds
+int waitOff = 500; // 0.5 Seconds
+int blinks = 4; // Chosen by dice roll, guaranteed to be random but should be overwritten by the random() function
+int i = 0; // A throwaway variable for our while loop
+
+void setup() {                
+  // Tell the ATTiny that we want to use pin 1 as an output
+  pinMode(ledPin, OUTPUT); // Our LED is pin 1 and we're supplying electricity to it.
+}
+
+// loop runs forever and ever:
+void loop() {
+  blinks = random(1,11);
+
+  for (i = 0; i < blinks; i++){
+    digitalWrite(ledPin, HIGH); // Make the LED turn on
+    delay(waitOn);  // wait
+    digitalWrite(ledPin, LOW); // Make the LED turn off
+    delay(waitOff);
+  }
+  
+  delay(numberDelay); // Wait between numbers
+}
